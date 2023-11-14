b4df8f6d99bdb9049ead4f50132fe47a3547803a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-14 15:25:20 -0500

    Adding default messege responses and redefined class responses

--- src/utils/res.ts
+++ src/utils/res.ts
@@ -1,32 +1,85 @@
 import { Response } from "express";
 
 class Responses {
-  success<T>(res: Response, key: string, datas: T[]): Response {
+  protected jsonResponse<T>(
+    res: Response,
+    key: string,
+    datas: T[] | T,
+    status: number | string
+  ): Response {
+    status = status == 200 ? "OK" : "KO";
     return res.status(200).json({
       [key]: datas,
-      status: "OK",
-    });
-  }
-  successWithSingleData<T>(res: Response, key: string, data: T): Response {
-    return res.status(200).json({
-      [key]: data,
-      status: "OK",
-    });
-  }
-  badRequest(res: Response, messege?: string): Response {
-    return res.status(400).json({
-      TypeError: "Bad Request",
-      messege: "",
-      status: "KO",
-    });
-  }
-  notFound(res: Response, messege?: string): Response {
-    return res.status(404).json({
-      TypeError: "Not Found",
-      messege: "",
-      status: "KO",
+      status,
     });
   }
 }
 
-export default Responses;
+export class SuccessResponses extends Responses {
+  sendSuccessWithMultipleData<T>(
+    res: Response,
+    key: string,
+    datas: T[]
+  ): Response {
+    return this.jsonResponse<T>(res, key, datas, 200);
+  }
+  sendSuccessSingleData<T>(res: Response, key: string, data: T): Response {
+    return this.jsonResponse<T>(res, key, data, 200);
+  }
+  success(res: Response, key: string): Response {
+    return res.status(200).json({
+      [key]: true,
+      status: "OK",
+    });
+  }
+}
+
+export class ErrorsResponses {
+  private ErrorResponse(
+    res: Response,
+    messege: string | undefined,
+    statusCode: number,
+    TypeError: string
+  ): Response {
+    return res.status(statusCode).json({
+      TypeError,
+      messege,
+      status: "KO",
+    });
+  }
+  badRequest(res: Response, messege?: string): Response {
+    return this.ErrorResponse(
+      res,
+      messege ||
+        "Oops! Your request cannot be processed due to a bad request. Please check your input and try again.",
+      400,
+      "Bad Request"
+    );
+  }
+  notFound(res: Response, messege?: string): Response {
+    return this.ErrorResponse(
+      res,
+      messege || "Oops! The data you are looking for could not be found.",
+      404,
+      "Not Found"
+    );
+  }
+  unprocessable(res: Response, messege?: string): Response {
+    return this.ErrorResponse(
+      res,
+      messege ||
+        "The content you provided could not be processed due to errors in the data. Please review your input and make sure it meets the required format and criteria.",
+      422,
+      "Unprocessable Content"
+    );
+  }
+  unauth(res: Response, messege?: string): Response {
+    return this.ErrorResponse(
+      res,
+      messege ||
+        "You are not authorized to access this resource. Please provide valid credentials or authentication.",
+      401,
+      "Unauthorized"
+    );
+  }
+}
