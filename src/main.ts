d2652856897ab2c2c3e6e2c9661245e87d7f2d73
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-14 05:40:21 -0500

    Adding routes for users API Endpoint

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,8 @@ app.use(fileUpload());
 app.set("trust proxy", 1);
 
 // Application Routes
+import usersRoutes from "./app/v1/users/routes/users.routes.ts";
+
+app.use("/v1/users", usersRoutes);
 
 export default app;
