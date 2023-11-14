0bdb0b6c02cd3c4817bda730ead4a56aeaf873f3
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-14 05:36:46 -0500

    Update status code of BadRequest and NotFound

--- src/utils/res.ts
+++ src/utils/res.ts
@@ -7,6 +7,26 @@ class Responses {
       status: "OK",
     });
   }
+  successWithSingleData<T>(res: Response, key: string, data: T): Response {
+    return res.status(200).json({
+      [key]: data,
+      status: "OK",
+    });
+  }
+  badRequest(res: Response, messege?: string): Response {
+    return res.status(400).json({
+      TypeError: "Bad Request",
+      messege: "",
+      status: "KO",
+    });
+  }
+  notFound(res: Response, messege?: string): Response {
+    return res.status(404).json({
+      TypeError: "Not Found",
+      messege: "",
+      status: "KO",
+    });
+  }
 }
 
 export default Responses;
