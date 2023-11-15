4f859357f6012c72e058fbe45423e3fd1b91b4ef
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-15 03:59:15 -0500

    Adding Update API Endpoint

--- src/app/v1/users/routes/users.routes.ts
+++ src/app/v1/users/routes/users.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { singleUser } from "../controllers/user.get.controller";
+import updateUser from "../controllers/user.update.controller";
 
 router.get("/:id", singleUser);
+router.patch("/:id", updateUser);
 
 export default router;
