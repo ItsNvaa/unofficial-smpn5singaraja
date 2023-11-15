8768439ddfd149dcbc672cbc591aee22b04c4c3b
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-15 05:41:02 -0500

    Adding Delete User API Endpoint and Delete User API Endpoint Unit-test

--- src/app/v1/users/routes/users.routes.ts
+++ src/app/v1/users/routes/users.routes.ts
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 import { singleUser } from "../controllers/user.get.controller";
 import updateUser from "../controllers/user.update.controller";
+import deleteUser from "../controllers/user.delete.controller";
 
 router.get("/:id", singleUser);
 router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
