8ec15069747b5d8492366f351fe422f45c0805ac
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-16 07:11:47 -0500

    Adding new feature: Update Achivement API Endpoint

--- src/app/v1/achievements/routes/achivement.routes.ts
+++ src/app/v1/achievements/routes/achivement.routes.ts
@@ -1,12 +1,14 @@
 import express from "express";
 const router = express.Router();
 import addAchivement from "../controllers/achivement.add.controller";
+import updateAchivement from "../controllers/achivement.update.controller";
 import {
   achivements,
   singleAchivement,
 } from "../controllers/achivement.get.controller";
 
 router.get("/", achivements);
+router.patch("/:id", updateAchivement);
 router.get("/:id", singleAchivement);
 router.post("/", addAchivement);
 
