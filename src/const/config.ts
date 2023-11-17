f6912350f4ae9f4e3b36a59c159d58fe0077c0b0
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-17 09:08:31 -0500

    Adding Json Web Token AccessToken and RefreshToken

--- src/const/config.ts
+++ src/const/config.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
 import process from "process";
 
-export const { PORT, DATABASE_URL } = process.env;
+export const { PORT, DATABASE_URL, JWT_ACCESS_TOKEN, JWT_REFRESH_TOKEN } =
+  process.env;
