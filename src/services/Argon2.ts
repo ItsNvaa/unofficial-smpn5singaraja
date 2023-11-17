1998f482fb48b54eaf18b7a73e37025b1d02cb2d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-17 08:41:31 -0500

    Refactoring some line of code

--- src/services/Argon2.ts
+++ src/services/Argon2.ts
@@ -2,14 +2,14 @@ import TUser from "../app/v1/users/interfaces/types/UserTypes";
 import logger from "../libs/logger";
 
 class Argon2 {
-  hash(password: string): string {
+  hash(password: string): string | null {
     try {
       const hash = Bun.password.hashSync(password, { algorithm: "argon2id" });
 
       return hash;
     } catch (err) {
       logger.error(err);
-      throw new Error("Hashing failed.");
+      return null;
     }
   }
   verify(input: TUser, password: string): boolean {
