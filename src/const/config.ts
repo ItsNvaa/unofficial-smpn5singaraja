db5d412962c6cfb7cc3f362bddf6d8e3b8d96195
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-17 09:36:39 -0500

    Adding CLIENT_URL

--- src/const/config.ts
+++ src/const/config.ts
@@ -1,5 +1,10 @@
 import "dotenv/config";
 import process from "process";
 
-export const { PORT, DATABASE_URL, JWT_ACCESS_TOKEN, JWT_REFRESH_TOKEN } =
-  process.env;
+export const {
+  PORT,
+  DATABASE_URL,
+  JWT_ACCESS_TOKEN,
+  JWT_REFRESH_TOKEN,
+  CLIENT_URL,
+} = process.env;
