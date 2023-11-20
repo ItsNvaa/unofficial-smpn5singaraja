4eacbf6a2b41c477d305a82df2c76aeb71ed4760
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-20 08:37:38 -0500

    Refactoring opsional field in TeacherTypes

--- src/app/v1/teachers/interfaces/types/TeacherTypes.d.ts
+++ src/app/v1/teachers/interfaces/types/TeacherTypes.d.ts
@@ -1,12 +1,12 @@
 type TeacherType = {
   id: string;
   fullname: string;
-  NIP: int;
+  NIP: number;
   gender: string;
   grade: string;
   class: string;
-  email?: string;
-  profile?: string;
+  email: string | null;
+  profile: string | null;
   picture: string;
 };
 
