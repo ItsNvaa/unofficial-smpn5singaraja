96f8d6a6787bb800a60c88195d53164a5fc3629f
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-20 04:04:51 -0500

    Adding new galery field validation

---
+++ src/validations/galeryValidation.ts
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export default function galery({ required }: { required: boolean }) {
+  const isRequired = required ? Joi.string().required() : Joi.string();
+  const galeryValidation = Joi.object({
+    title: isRequired,
+    description: isRequired,
+    picture: isRequired,
+  });
+
+  return galeryValidation;
+}
