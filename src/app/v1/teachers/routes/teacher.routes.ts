75318090f70caeb8dfd7dd6e916e3b17c67f8bcc
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-21 01:24:40 -0500

    Adding new feature: Add Teacher API Endpoint

--- src/app/v1/teachers/routes/teacher.routes.ts
+++ src/app/v1/teachers/routes/teacher.routes.ts
@@ -4,10 +4,12 @@ import {
   singeTeacher,
   searchTeachers,
 } from "../controllers/teacher.get.controller";
+import addTeacher from "../controllers/teacher.add.controller";
 const router = express.Router();
 
 router.get("/", teachers);
 router.get("/search", searchTeachers);
 router.get("/:id", singeTeacher);
+router.post("/", addTeacher);
 
 export default router;
