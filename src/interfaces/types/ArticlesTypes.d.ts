63eb9525f7a20c33b84d91339eadf2978c343291
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-22 00:31:56 -0500

    Adding new types for features/news and features/articles

---
+++ src/interfaces/types/ArticlesTypes.d.ts
@@ -0,0 +1,11 @@
+type TArticle = {
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  picture: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export default TArticle;
