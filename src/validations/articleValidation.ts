651233f68e76202610d833a6af8874f66d22b951
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-22 00:52:05 -0500

    Adding article field validation

---
+++ src/validations/articleValidation.ts
@@ -0,0 +1,17 @@
+import Joi from "joi";
+import TArticle from "../interfaces/types/ArticlesTypes";
+
+export default function article({ required = true }: { required: boolean }) {
+  const isRequired = required ? Joi.string().required() : Joi.string();
+
+  const articleValidation = Joi.object<TArticle>({
+    title: isRequired,
+    description: isRequired,
+    author: isRequired,
+    picture: Joi.string(),
+    createdAt: Joi.string(),
+    updatedAt: Joi.string(),
+  });
+
+  return articleValidation;
+}
