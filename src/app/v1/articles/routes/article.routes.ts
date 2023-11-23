39902c3d4b73ef6b794cbf6e78632c3cf8bd7f2c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-23 03:22:53 -0500

    Adding new feature: Delete Article API Endpoint

--- src/app/v1/articles/routes/article.routes.ts
+++ src/app/v1/articles/routes/article.routes.ts
@@ -5,11 +5,13 @@ import {
 } from "../controllers/article.get.controller";
 import addArticle from "../controllers/article.add.controller";
 import updateArticle from "../controllers/article.update.controller";
+import deleteArticle from "../controllers/article.delete.controller";
 const router = express.Router();
 
 router.get("/", articles);
 router.get("/search", searchArticles);
 router.post("/", addArticle);
 router.patch("/:id", updateArticle);
+router.delete("/:id", deleteArticle);
 
 export default router;
