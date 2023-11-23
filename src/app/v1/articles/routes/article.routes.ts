a5733a424ea10796628e0102622ca0ed8aca0032
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-11-23 03:09:30 -0500

    Adding new feature: Update Article API Endpoint

--- src/app/v1/articles/routes/article.routes.ts
+++ src/app/v1/articles/routes/article.routes.ts
@@ -4,10 +4,12 @@ import {
   searchArticles,
 } from "../controllers/article.get.controller";
 import addArticle from "../controllers/article.add.controller";
+import updateArticle from "../controllers/article.update.controller";
 const router = express.Router();
 
 router.get("/", articles);
 router.get("/search", searchArticles);
 router.post("/", addArticle);
+router.patch("/:id", updateArticle);
 
 export default router;
